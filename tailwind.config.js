d7c8911b4c0ae92594a2af01f96db9176037b8e1
Sam <sam@samuels-MacBook-Air.local>
2025-04-29 02:36:11 +0900

    Fix PostCSS and Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./client/index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
